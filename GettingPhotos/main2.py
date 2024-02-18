feb62ddfe099f6aede737b070a7eecbab4307e1b
GerardoArredondo <gerardoijgv11@gmail.com>
2024-02-18 14:55:46 -0600

    Works with google photos

--- GettingPhotos/main2.py
+++ GettingPhotos/main2.py
@@ -16,42 +16,30 @@ def gettinImages(wd, delay, maxImages):
         wd.execute_script("window.scrollTo(0,document.body.scrollHeight);")
         time.sleep(delay)
 
-    #url = "https://twitter.com/home"
     url = "https://www.google.com/search?sca_esv=cd5515df3b07dc25&sxsrf=ACQVn082yViQP3i1TglnfFEU-uThXU7I6Q:1708285442500&q=dogs&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjApuaX07WEAxWcJ0QIHUo0AmYQ0pQJegQIDRAB&biw=1900&bih=897"
     wd.get(url)
 
     image_urls = set()
-    skips = 0
+    thumbnails = wd.find_elements(By.CLASS_NAME, "rg_i")
 
-
-    while len(image_urls)+skips< maxImages:
-        scroll(wd)
-        thumbnails = wd.find_elements(By.CLASS_NAME, "fR600b")
-        #thumbnails = wd.find_elements(By.CLASS_NAME, "css-175oi2r")
-
-
-        for img in thumbnails[len(image_urls) + skips: maxImages]:
-            try:
-                img.click()
-                time.sleep(delay)
-            except:
-                continue
+    for img in thumbnails[:maxImages]:
+        try:
+            img.click()
+            time.sleep(delay)
+        except:
+            continue
         
-            images = wd.find_elements(By.CLASS_NAME, "Q4LuWd")
-            #images = wd.find_elements(By.CLASS_NAME, "css-9pa8cd")
+        images = wd.find_elements(By.CLASS_NAME, "n3VNCb")
+        for image in images:
+            if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+                image_urls.add(image.get_attribute('src'))
+                print(f"Se encontró una imagen número {len(image_urls)}")
 
-            for image in images:
-                if image.get_attribute('src') in image_urls:
-                    maxImages += 1
-                    skips += 1
-                    break;
+            if len(image_urls) >= maxImages:
+                return image_urls
 
-
-
-                if image.get_attribute('src') and 'http' in image.get_attribute('src'):
-                    image_urls.add(image.get_attribute('src'))
-                    print(f"Se encontró una imagen numero {len(image_urls)}")
     return image_urls
+
 
 
 
@@ -78,6 +66,5 @@ for i, url in enumerate(urls):
 
 
 print(urls)
-
 
 
