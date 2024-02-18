599d370ec6f470af791876f92cf58bb621ea1d6a
GerardoArredondo <gerardoijgv11@gmail.com>
2024-02-18 14:21:12 -0600

    Works, but i need to fix the maxImages

--- GettingPhotos/main.py
+++ GettingPhotos/main.py
@@ -1,14 +1,54 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import requests
 import io
 from PIL import Image
+import time
 
 #PATH = "/home/gerotota/Udemy/Python/Projects/GettingPhotos/chromedriver.exe"
 
 wd = webdriver.Chrome()
 wd.get("https://www.google.com/")
 
-image_url = "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg"
+
+def gettinImages(wd, delay, maxImages):
+    def scroll(wd):
+        wd.execute_script("window.scrollTo(0,document.body.scrollHeight);")
+        time.sleep(delay)
+
+    #    url = "https://twitter.com/home?lang=en"
+    url = "https://www.google.com/search?sca_esv=cd5515df3b07dc25&sxsrf=ACQVn082yViQP3i1TglnfFEU-uThXU7I6Q:1708285442500&q=dogs&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjApuaX07WEAxWcJ0QIHUo0AmYQ0pQJegQIDRAB&biw=1900&bih=897"
+    wd.get(url)
+
+    image_urls = set()
+    skips = 0
+
+
+    while len(image_urls)+skips< maxImages:
+        scroll(wd)
+        thumbnails = wd.find_elements(By.CLASS_NAME, "fR600b")
+
+        for img in thumbnails[len(image_urls) + skips: maxImages]:
+            try:
+                img.click()
+                time.sleep(delay)
+            except:
+                continue
+        
+            images = wd.find_elements(By.CLASS_NAME, "Q4LuWd")
+            for image in images:
+                if image.get_attribute('src') in image_urls:
+                    maxImages += 1
+                    skips += 1
+                    break;
+
+
+
+                if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+                    image_urls.add(image.get_attribute('src'))
+                    print(f"Se encontró una imagen numero {len(image_urls)}")
+    return image_urls
+
 
 
 def downloadImage(download_path, url, file_name):
@@ -26,4 +66,14 @@ def downloadImage(download_path, url, file_name):
     except Exception as e:
         print("Algo fallo - ", e)
 
-downloadImage("", image_url, "test.jpg")
+urls = gettinImages(wd, 1, 5)
+
+
+for i, url in enumerate(urls):
+    downloadImage("Photos/", url, str(i)+".jpg")
+
+
+print(urls)
+
+
+
